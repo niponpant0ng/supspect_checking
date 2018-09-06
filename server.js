27c309b1e6f5787e463c8db2829c0c7bf1c5168b
nipon pantong <nipon.pantong@scb.co.th>
2018-09-06 23:49:53 +0700

    Add upload route

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const next = require("next")
 const express = require("express")
+const multer = require("multer")
+const del = require("del")
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
+
+const upload = multer({ dest: `tmp/` })
 
 const routes = require('./routes')
 const handle = routes.getRequestHandler(app)
 
 app.prepare()
   .then(() => {
-    express()
+    const server = express()
+
+    server.post("/upload", upload.single('file'), async (req, res) => {
+      try {
+        res.send("Upload Complete")
+      } catch(err) {
+        res.sendStatus(500)
+      }
+    })
+
+    server
       .use(handle)
       .listen(3003)
   })
