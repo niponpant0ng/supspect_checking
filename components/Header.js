fa0fb7c2f16691b1e1c9d7c509555fc160b4b58c
nipon pantong <nipon.pantong@scb.co.th>
2018-09-09 18:16:01 +0700

    Style search page

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,18 @@
 import Link from "next/link"
 
+const menuStyle = {
+  width: "100%",
+  height: 50,
+  padding: 10,
+  backgroundColor: "#eee",
+  borderRadius: 5,
+  marginBottom: 20
+}
+
 export default (props) => (
-  <nav className="navbar navbar-default">
-    <div className="container-fluid">
-      <div className="collapse navbar-collapse">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link href={`/`}><a>นำเข้าข้อมูล</a></Link>
-          </li>
-          <li>
-            <Link href={`/search`}><a>ค้นหาข้อมูล</a></Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <div style={ menuStyle }>
+    <Link href={`/`}><a>นำเข้าข้อมูล</a></Link>
+    <div style={{ display: "inline-block", marginLeft: 10, marginRight: 10 }}>|</div>
+    <Link href={`/search`}><a>ค้นหาข้อมูล</a></Link>
+  </div>
 )
--- pages/search.js
+++ pages/search.js
@@ -63,41 +63,71 @@ export default class Search extends React.Component {
 
     return (
       <Layout>
-        วันที่จาก: <input value={ dateFrom } onChange={ this.onFilterChanged.bind(null, 'dateFrom') }/>
-        วันที่ถึง: <input value={ dateTo } onChange={ this.onFilterChanged.bind(null, 'dateTo') }/>
-        เวลาจาก: <input value={ timeFrom } onChange={ this.onFilterChanged.bind(null, 'timeFrom') }/>
-        เวลาถึง: <input value={ timeTo } onChange={ this.onFilterChanged.bind(null, 'timeTo') }/>
-        ทะเบียนรถ: <input value={ plateNo } onChange={ this.onFilterChanged.bind(null, 'plateNo') }/>
-        หมายเลขบัตรประชาชน: <input value={ idNo } onChange={ this.onFilterChanged.bind(null, 'idNo') }/>
-        <p><button onClick={ this.onSearchClicked }>ค้นหา</button></p>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4 form-group">
+              <div>วันที่</div>
+              <input value={ dateFrom } onChange={ this.onFilterChanged.bind(null, 'dateFrom') }/> - <input value={ dateTo } onChange={ this.onFilterChanged.bind(null, 'dateTo') }/>
+            </div>
 
-        <table border="1" cellPadding="0" cellSpacing="0">
-          <thead>
-            <tr>
-              <td>date</td>
-              <td>time</td>
-              <td>police_area</td>
-              <td>requester</td>
-              <td>checking_area</td>
-              <td>car_register</td>
-              <td>car_owner</td>
-              <td>motorcycle_register</td>
-              <td>motorcycle_owner</td>
-              <td>color</td>
-              <td>brand</td>
-              <td>model</td>
-              <td>type</td>
-              <td>id_no</td>
-              <td>name</td>
-              <td>result</td>
-              <td>checking</td>
-              <td>detail</td>
-            </tr>
-          </thead>
-          <tbody>
-            { rows }
-          </tbody>
-        </table>
+            <div className="col-4 form-group">
+              <div>เวลา</div>
+              <input value={ timeFrom } onChange={ this.onFilterChanged.bind(null, 'timeFrom') }/> - <input value={ timeTo } onChange={ this.onFilterChanged.bind(null, 'timeTo') }/>
+            </div>
+
+            <div className="col-4 form-group">
+              <div>ทะเบียนรถ</div>
+              <input value={ plateNo } onChange={ this.onFilterChanged.bind(null, 'plateNo') }/>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-4 form-group">
+              <div>หมายเลขบัตรประชาชน</div>
+              <input value={ idNo } onChange={ this.onFilterChanged.bind(null, 'idNo') }/>
+            </div>
+
+            <div className="col-4 form-group">
+              <button className="btn btn-primary" onClick={ this.onSearchClicked }>ค้นหา</button>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="table-responsive">
+              <table className="table table-striped" style={{ width: "auto" }}>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col" rowSpan="2">วันที่</th>
+                    <th scope="col" rowSpan="2">เวลา</th>
+                    <th scope="col" rowSpan="2">สภ</th>
+                    <th scope="col" rowSpan="2">ผู้ขอข้อมูล</th>
+                    <th scope="col" rowSpan="2">จุดตรวจ</th>
+                    <th scope="col" colSpan="2">จยย</th>
+                    <th scope="col" colSpan="2">รถยนต์</th>
+                    <th scope="col" rowSpan="2">สี</th>
+                    <th scope="col" rowSpan="2">ยี่ห้อ</th>
+                    <th scope="col" rowSpan="2">รุ่น</th>
+                    <th scope="col" rowSpan="2">ประเภท</th>
+                    <th scope="col" rowSpan="2">หมายเลข 13 หลัก</th>
+                    <th scope="col" rowSpan="2">บุคคล(ชื่อ/สกุล)</th>
+                    <th scope="col" rowSpan="2">ผลการตรวจ</th>
+                    <th scope="col" rowSpan="2">ภ.จว.ที่ขอตรวจ</th>
+                    <th scope="col" rowSpan="2">ข้อมูลที่สมบูรณ์</th>
+                  </tr>
+                  <tr>
+                    <th scope="col">ทะเบียน</th>
+                    <th scope="col">ผู้ขับขี่/ครอบครอง</th>
+                    <th scope="col">ทะเบียน</th>
+                    <th scope="col">ผู้ขับขี่/ครอบครอง</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { rows }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </Layout>
     )
   }
